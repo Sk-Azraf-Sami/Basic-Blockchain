f60ada1c4987ec4eaacad3591c8015ad9ef53e6a
Sk-Azraf-Sami <azraf7026official@gmail.com>
2023-03-01 21:31:13 +0600

    Mine Blocks

--- index.js
+++ index.js
@@ -8,10 +8,21 @@ class Block {
         this.data = data;
         this.previousHash = previousHash; 
         this.hash = this.calculateHash(); 
+        this.nonce=0; 
+    }
+
+    mineBlock(difficulty){
+        while(this.hash.substring(0,difficulty) !== Array(difficulty+1).join("0")){
+            this.nonce++;
+            this.hash=this.calculateHash();
+        }
     }
 
     calculateHash(){
-        return sha256 (this.timestamp + JSON.stringify(this.data) + this.previousHash).toString();
+        return sha256 (this.timestamp +
+             JSON.stringify(this.data) + 
+             this.previousHash + 
+             this.nonce).toString();
     }
 }
 
@@ -60,3 +71,5 @@ console.log(josscoin.checkValidationofBlock());
 console.log("After Invalid Change of Data");
 josscoin.chain[1].data="HACKED";
 console.log(josscoin.checkValidationofBlock());
+
+//console.log(Array(6).join("0"));
