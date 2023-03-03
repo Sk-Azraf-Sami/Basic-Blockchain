3ca1886127f8e41f77ef82b7b6edce8ee3149074
Sk-Azraf-Sami <azraf7026official@gmail.com>
2023-03-03 10:56:56 +0600

    Creating keygen.js for generating public and private key

---
+++ keygen.js
@@ -0,0 +1,9 @@
+const EC = require("elliptic").ec;
+var ec = new EC("secp256k1");
+
+//Generate Key 
+var key = ec.genKeyPair();
+const privateKey = key.getPrivate("hex"); //signature or sign
+const publicKey = key.getPublic("hex");  // wallet address 
+console.log("Private key: ",privateKey);
+console.log("Public Key: ",publicKey); 
